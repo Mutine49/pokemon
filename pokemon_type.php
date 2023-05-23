40bcb26d5abba2669546e72d91949ec6e466f0fa
LAPTOP-HNT8QJJ0\JNL <ddamienneyron@gmail.com>
2023-05-23 11:05:55 +0200

    on a le droit d'envoyer ouuuu ???

---
+++ pokemon_type.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mon premier site web</title>
+</head>
+<body>
+  <header>
+    <h1>Titre de mon site</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <h2>Contenu principal</h2>
+    <p>Bienvenue sur mon site web!</p>
+  </main>
+
+  <footer>
+    <p>&copy; 2023 Mon site web. Tous droits réservés.</p>
+  </footer>
+</body>
+</html>
