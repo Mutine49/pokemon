4ce4db18fc06f6bb05a32d6f2bbafda3d18ea550
LAPTOP-4VKI56BK\Evan.c <evan.clearque@gmail.com>
2023-05-23 11:56:11 +0200

    no message

--- search_trainer.php
+++ search_trainer.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +6,34 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>recherche dresseur</title>
 </head>
+<style type="text/css">
+
+</style>
+
 <body>
+	<?php
+    include_once('header.php')
+?>
+<h1>Liste de dresseur pokemon</h1>
+<nav>
+      <ul>
+        <li><a href="#">Théodore</a></li>
+        <li><a href="#">Evan</a></li>
+        <li><a href="#">Trystan</a></li>
+        <li><a href="#">Arthus</a></li>
+        <li><a href="#">Tom</a></li>
+        <li><a href="#">Aymeric</a></li>
+        <li><a href="#">Alban</a></li>
+        <li><a href="#">Damien</a></li>
+        <li><a href="#">Colin</a></li>
+        <li><a href="#">Cyprien</a></li>
+      </ul>
+    </nav>
+
+<div>
+	
+</div>
+
 
 </body>
 </html>
