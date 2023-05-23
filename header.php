3b1a9818f0c01698ee8c485fbb7e0d2661298eb8
WORKLAPTOP\thust <arthus.dubois@lecole-ldlc.com>
2023-05-23 11:18:49 +0200

    toutes les pages + modif header

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="search_type.php">Types</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="search_trainer.php">Dresseurs</a>
+        </li>
       </ul>
     </div>
   </div>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de recettes - Page d'accueil</title>
+    <title>Pokémon - Page d'accueil</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
@@ -12,6 +12,6 @@
 </head>
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
-<?php
-    include_once('header.php')
-?>
+    
+    <?php include_once('header.php') ?>
+</body>
