dc81901018de64abfb5674e509b86d52d8fd4009
TRYSTAN\tryst <trystangoug@gmail.com>
2023-05-23 11:57:09 +0200

    derniere version

--- pokemon_perso.php
+++ pokemon_perso.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Pokemon perso</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="pok_perso.css" rel="stylesheet">
+    <title>Pokemon perso</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
-<body>
-rgergr
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+    
+    <?php include_once('header.php') ?>
+<br>
+<h1><u>Caractéristiques du Pokémon : </u></h1>
 </body>
-</html>
