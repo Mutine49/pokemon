ebf4a5b6ed9230e5878d981ed8b54f2db0a00e57
LAPTOP-AEB1MH9M\matth <matthias.exbrayat@yahoo.fr>
2023-05-23 11:43:07 +0200

    no message

--- search_type.php
+++ search_type.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>recherche type</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
-<body>
-
-</body>
-</html>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
 <?php
     include_once('header.php')
 ?>
