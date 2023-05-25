f1598a5213274df9c93a1c7dd655a477d1666c16
LAPTOP-4VKI56BK\Evan.c <evan.clearque@gmail.com>
2023-05-25 11:52:48 +0200

    search trainer

--- search_trainer.php
+++ search_trainer.php
@@ -1,3 +1,4 @@
+<?php include_once('config/mysql.php') ?>
 
 <!DOCTYPE html>
 <html>
@@ -6,34 +7,57 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>recherche dresseur</title>
 </head>
+<link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 <style type="text/css">
+    table{
+      border-collapse: collapse;
+      width: 400px;
+}
+    th, td{
+      border: 1px solid black;
+      padding: 10px;
+      vertical-align: top;
+}
+    </style>
 
-</style>
 
-<body>
-	<?php
-    include_once('header.php')
-?>
+<body class="d-flex flex-column min-vh-100">
+	<div class="container">
+        <?php include_once('header.php') ?>   
+    </div>
+
 <h1>Liste de dresseur pokemon</h1>
-<nav>
-      <ul>
-        <li><a href="#">Théodore</a></li>
-        <li><a href="#">Evan</a></li>
-        <li><a href="#">Trystan</a></li>
-        <li><a href="#">Arthus</a></li>
-        <li><a href="#">Tom</a></li>
-        <li><a href="#">Aymeric</a></li>
-        <li><a href="#">Alban</a></li>
-        <li><a href="#">Damien</a></li>
-        <li><a href="#">Colin</a></li>
-        <li><a href="#">Cyprien</a></li>
-      </ul>
-    </nav>
-
-<div>
-	
+        
+<div class="card">
+    
+    <div class="card-body">
+        <h5 class="card-title">Rappel de vos informations</h5>
+        <p class="card-text"><b>liste de dresseur</b> : </p>
+        <table>
+          <thead>
+            <tr>
+              <th>nom</th>
+              <th>Nombre de combat</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> <a href ='#'> dresseur 1 <?php echo $_GET['dresseur']; ?></td>
+              <td>nombre de combat du dresseur 1</td>
+            </tr>
+          </tbody>
+        </table>
+        <?php echo $_GET['dresseur']; ?>
+    </div>
 </div>
 
 
+   
+
+
+<?php include_once('footer.php') ?>
 </body>
 </html>
